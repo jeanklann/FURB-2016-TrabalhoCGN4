f3d1279088dad7d065717c8313bb085754b7b914
Lucas Schaefer <lucas.schaefer@hotmail.com.br>
2016-06-07 19:37:48 -0300

    GUI

---
+++ CGN4/GUI.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+namespace CGN4
+{
+    public class GUI
+    {
+        private readonly Font TextFont = new Font(FontFamily.GenericSansSerif, 20);
+        private readonly Dictionary<string, GUIText> texts;
+        private readonly List<GUIShape> shapes;
+        private Bitmap TextBitmap;
+        private int textureId;
+        private Size clientSize;
+        private Rectangle clientRectangle;
+
+        public void Resize(Rectangle clientRectangle, Size clientSize)
+        {
+            foreach (var text in texts.Values.Where(p => p.MustResize))
+            {
+                var newX = text.Position.X + ((clientSize.Width - this.clientSize.Width) / 2);
+                var newY = text.Position.Y + ((clientSize.Height - this.clientSize.Height) / 2);
+                text.Position = new PointF(newX, newY);
+            }
+
+            this.clientRectangle = clientRectangle;
+            this.clientSize = clientSize;
+            TextBitmap = new Bitmap(clientRectangle.Width, clientRectangle.Height);
+            CreateTexture();
+        }
+
+        private void CreateTexture()
+        {
+            int newTextureId;
+            GL.TexEnv(TextureEnvTarget.TextureEnv, TextureEnvParameter.TextureEnvMode, (float)TextureEnvMode.Replace);
+            Bitmap bitmap = TextBitmap;
+            GL.GenTextures(1, out newTextureId);
+            GL.BindTexture(TextureTarget.Texture2D, newTextureId);
+
+            BitmapData data = bitmap.LockBits(new Rectangle(0, 0, bitmap.Width, bitmap.Height), ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, data.Width, data.Height, 0, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D, TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            GL.Finish();
+            bitmap.UnlockBits(data);
+            textureId = newTextureId;
+        }
+
+        public GUI(Rectangle clientRectangle, Size clientSize)
+        {
+            texts = new Dictionary<string, GUIText>();
+            shapes = new List<GUIShape>();
+            Resize(clientRectangle, clientSize);
+        }
+
+        public void ClearGUI()
+        {
+            texts.Clear();
+            shapes.Clear();
+        }
+
+        public void AddText(GameState gameState, string key, string s, PointF pos, Brush col, bool mustResize = false)
+        {
+            texts.Add(key, new GUIText
+            {
+                GameState = gameState,
+                Color = col,
+                Line = s,
+                Position = pos,
+                MustResize = mustResize
+            });
+        }
+
+        public void UpdateText(string key, string s)
+        {
+            if (texts.ContainsKey(key))
+            {
+                texts[key].Line = s;
+            }
+        }
+
+        public void AddShape(GUIShape shape)
+        {
+            shapes.Add(shape);
+        }
+
+        public void Update(GameState gameState)
+        {
+            //Faz update nas texturas de texto
+            if (texts.Count > 0)
+            {
+                using (Graphics gfx = Graphics.FromImage(TextBitmap))
+                {
+                    gfx.Clear(Color.Black);
+                    gfx.TextRenderingHint = System.Drawing.Text.TextRenderingHint.ClearTypeGridFit;
+                    for (int i = 0; i < texts.Count; i++)
+                    {
+                        var text = texts.Values.ToArray()[i];
+                        if (text.GameState == gameState)
+                        {
+                            gfx.DrawString(text.Line, TextFont, text.Color, text.Position);
+                        }
+                    }
+                }
+
+                BitmapData data = TextBitmap.LockBits(new Rectangle(0, 0, TextBitmap.Width, TextBitmap.Height),
+                ImageLockMode.ReadOnly, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+                GL.TexSubImage2D(TextureTarget.Texture2D, 0, 0, 0, TextBitmap.Width, TextBitmap.Height, OpenTK.Graphics.OpenGL.PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+                TextBitmap.UnlockBits(data);
+            }
+        }
+
+        public void Draw(GameState gameState)
+        {
+            DrawShapes(gameState);
+            DrawText();
+        }
+
+        private void DrawText()
+        {
+            GL.PushMatrix();
+            GL.Disable(EnableCap.DepthTest);
+            GL.LoadIdentity();
+            GL.Viewport(new Rectangle(clientRectangle.Left, clientRectangle.Top, clientRectangle.Width, clientRectangle.Height));
+            Matrix4 ortho_projection = Matrix4.CreateOrthographicOffCenter(0, clientSize.Width, clientSize.Height, 0, -35, 1);
+            GL.MatrixMode(MatrixMode.Projection);
+
+            GL.PushMatrix();
+            GL.LoadMatrix(ref ortho_projection);
+
+            GL.Enable(EnableCap.Blend);
+            GL.BlendFunc(BlendingFactorSrc.One, BlendingFactorDest.DstColor);
+            GL.Enable(EnableCap.Texture2D);
+            GL.BindTexture(TextureTarget.Texture2D, textureId);
+
+            GL.Begin(PrimitiveType.Quads);
+            GL.TexCoord2(0, 0); GL.Vertex2(0, 0);
+            GL.TexCoord2(1, 0); GL.Vertex2(TextBitmap.Width, 0);
+            GL.TexCoord2(1, 1); GL.Vertex2(TextBitmap.Width, TextBitmap.Height);
+            GL.TexCoord2(0, 1); GL.Vertex2(0, TextBitmap.Height);
+            GL.End();
+            GL.PopMatrix();
+
+            GL.Enable(EnableCap.DepthTest);
+            GL.Disable(EnableCap.Blend);
+            GL.Disable(EnableCap.Texture2D);
+
+            GL.PopMatrix();
+        }
+
+        private void DrawShapes(GameState gameState)
+        {
+            GL.PushMatrix();
+            GL.LoadIdentity();
+            foreach (var shape in shapes.Where(p => p.GameState == gameState))
+            {
+                GL.Begin(PrimitiveType.Quads);
+                GL.Color3(shape.Color);
+                GL.LineWidth(4);
+                GL.Vertex2(shape.X, shape.Y);
+                GL.Vertex2(shape.X + 2f, shape.Y);
+                GL.Vertex2(shape.X + 2f, shape.Y - 2f);
+                GL.Vertex2(shape.X, shape.Y - 2f);
+                GL.End();
+            }
+
+            GL.PopMatrix();
+        }
+
+        public void Dispose()
+        {
+            if (textureId > 0)
+            {
+                GL.DeleteTexture(textureId);
+            }
+        }
+    }
+
+    public class GUIShape
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        public GameState GameState { get; set; }
+        public PrimitiveType Primitive { get; set; }
+        public Color Color { get; set; }
+    }
+
+    public class GUIText
+    {
+        public bool MustResize { get; set; }
+        public GameState GameState { get; set; }
+        public PointF Position { get; set; }
+        public string Line { get; set; }
+        public Brush Color { get; set; }
+    }
+}
--- CGN4/Game.cs
+++ CGN4/Game.cs
@@ -3,27 +3,29 @@ using OpenTK.Graphics.OpenGL;
 using OpenTK.Input;
 using System.Drawing;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using ObjLoader.Loader.Loaders;
 using System.IO;
 
-namespace CGN4 {
-    class Game:GameWindow {
+namespace CGN4
+{
+    class Game : GameWindow
+    {
         public Nave nave;
         public CriadorDePredios predios;
+        public GUI GUI;
         public Matrix4d ProjectionMatrix;
         public static Game MainGame;
         public int score = 0;
+
+        public GameState gameState = GameState.Stopped;
 
         float[] mat_specular = { 1.0f, 1.0f, 1.0f, 1.0f };
         float[] mat_shininess = { 50.0f };
         float[] light_position = { 1.0f, 1.0f, 1.0f, 0.0f };
         float[] light_ambient = { 0.5f, 0.5f, 0.5f, 1.0f };
 
-        protected override void OnLoad(EventArgs e) {
+        protected override void OnLoad(EventArgs e)
+        {
             base.OnLoad(e);
             GL.ClearColor(Color.Black);
             GL.ShadeModel(ShadingModel.Smooth);
@@ -40,6 +42,13 @@ namespace CGN4 {
             GL.EnableClientState(ArrayCap.VertexArray);
             GL.EnableClientState(ArrayCap.ColorArray);
 
+            InitGame();
+        }
+
+        private void InitGame()
+        {
+            GUI = new GUI(ClientRectangle, ClientSize);
+
             var objLoaderFactory = new ObjLoaderFactory();
             var objLoader = objLoaderFactory.Create();
             var fileStream = new FileStream("predio.obj", FileMode.Open);
@@ -52,52 +61,136 @@ namespace CGN4 {
             fileStream = new FileStream("nave.obj", FileMode.Open);
             result = objLoader.Load(fileStream);
             Nave.vbo = new VBO(result);
-
-
+            fileStream.Close();
             nave = new Nave();
             nave.Position = new Vector3d(0, 0, -2);
 
-            
-            //Predio.Load();
             predios = new CriadorDePredios(nave);
             MainGame = this;
+
+            #region Define GUI
+
+            GUI.AddText(GameState.Running, "ScoreR", "Score: 0", new PointF(0, 0), new SolidBrush(Color.White));
+
+            GUI.AddText(GameState.Paused, "Texto jogo pausado", "Jogo pausado", new PointF(110, 100), new SolidBrush(Color.White), true);
+            GUI.AddText(GameState.Paused, "ScoreP", "Score: 0", new PointF(140, 150), new SolidBrush(Color.White), true);
+            GUI.AddText(GameState.Paused, "Texto continuar", "Pressione P para continuar.", new PointF(30, 200), new SolidBrush(Color.White), true);
+            GUI.AddShape(new GUIShape
+            {
+                GameState = GameState.Paused,
+                X = -1f,
+                Y = 1,
+                Color = Color.CadetBlue,
+                Primitive = PrimitiveType.QuadStrip
+            });
+
+            GUI.AddText(GameState.Defeat, "Texto derrota", "Você morreu", new PointF(110, 100), new SolidBrush(Color.White), true);
+            GUI.AddText(GameState.Defeat, "ScoreD", "Score: " + score, new PointF(140, 150), new SolidBrush(Color.White), true);
+            GUI.AddText(GameState.Defeat, "Texto recomeçar", "Pressione R para recomeçar.", new PointF(30, 200), new SolidBrush(Color.White), true);
+            GUI.AddShape(new GUIShape
+            {
+                GameState = GameState.Defeat,
+                X = -1f,
+                Y = 1,
+                Color = Color.Salmon,
+                Primitive = PrimitiveType.QuadStrip
+            });
+
+            #endregion Define GUI
+
+            gameState = GameState.Running;
         }
-        protected override void OnResize(EventArgs e) {
+
+        private void RestartGame()
+        {
+            score = 0;
+            nave = new Nave();
+            nave.Position = new Vector3d(0, 0, -2);
+            predios = new CriadorDePredios(nave);
+            gameState = GameState.Running;
+        }
+
+        private void PauseGame()
+        {
+            if (gameState == GameState.Running)
+            {
+                gameState = GameState.Paused;
+            }
+            else
+            {
+                if (gameState == GameState.Paused)
+                {
+                    gameState = GameState.Running;
+                }
+            }
+        }
+
+        protected override void OnResize(EventArgs e)
+        {
             GL.Viewport(new Rectangle(ClientRectangle.Left, ClientRectangle.Top, ClientRectangle.Width, ClientRectangle.Height));
+
             ProjectionMatrix = Matrix4d.Perspective(30, ClientSize.Width / ClientSize.Height, 1, 100);
             GL.MatrixMode(MatrixMode.Projection);
             GL.LoadMatrix(ref ProjectionMatrix);
-            Console.WriteLine("Janela redimensionada");
+            GUI.Resize(ClientRectangle, ClientSize);
+            Console.WriteLine("Janela e GUI redimensionada");
         }
-        protected override void OnUpdateFrame(FrameEventArgs e) {
-            predios.Update();
-            nave.Update();
-        }
-        protected override void OnRenderFrame(FrameEventArgs e) {
-            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
-            GL.PushMatrix();
-            #region Camera
-            GL.Scale(new Vector3d(1,-1,1));
-            #endregion
 
-            #region Draw
-            
-            nave.Draw();
-            predios.Draw();
-            #endregion
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            GUI.Update(gameState);
+            GUI.UpdateText("ScoreR", "Score: " + score);
+            GUI.UpdateText("ScoreP", "Score: " + score);
+            GUI.UpdateText("ScoreD", "Score: " + score);
+            if (gameState == GameState.Running)
+            {
+                predios.Update();
+                nave.Update();
+            }
+        }
+
+        private bool isRunning = true;
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            GUI.Draw(gameState);
+            GL.PushMatrix();
+
+            if (gameState == GameState.Running)
+            {
+                #region Camera
+
+                GL.Scale(new Vector3d(1, -1, 1));
+
+                #endregion
+
+                #region Draw
+
+                nave.Draw();
+                predios.Draw();
+
+                #endregion
+            }
 
             GL.PopMatrix();
             GL.Finish();
             SwapBuffers();
         }
-        public Game() {
-            KeyDown += (object sender, KeyboardKeyEventArgs e) => {
-                switch(e.Key) {
-                    case Key.R:
-                        
+        public Game()
+        {
+            KeyDown += (object sender, KeyboardKeyEventArgs e) =>
+            {
+                switch (e.Key)
+                {
+                    case Key.D:
+                        gameState = GameState.Defeat;
                         break;
-                    
-
+                    case Key.R:
+                        RestartGame();
+                        break;
+                    case Key.P:
+                        PauseGame();
+                        break;
                 }
             };
         }
